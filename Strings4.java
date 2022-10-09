26919711ec8888ac1e7438e760e3c5616fce8a99
Kumar Swamy N <104250670+KumarSN1996@users.noreply.github.com>
2022-10-09 15:54:13 +0530

    Update Strings4.java

--- Strings4.java
+++ Strings4.java
@@ -2,4 +2,33 @@ package Udemy;
 
 public class Strings4 {
 
+	public static void main(String[] args) {
+		String sentence1 = "inueron";
+
+		String sentence2 = "the five boxing wizards jump quickly"; // is a pangram
+
+		System.out.println("INueron is Pangram : " + isPangram(sentence1));
+
+		System.out.println("Sentence2  is Pangram : " + isPangram(sentence2));
+	}
+
+	static boolean isPangram(String str) {
+
+		for (int i = 97; i <= 122; i++) {
+			boolean flag = false;
+
+			for (int index = 0; index < str.length(); index++) {
+
+				if ((int) str.charAt(index) == i) {
+					flag = true;
+					break;
+				}
+
+			}
+			if (flag == false)
+				return false;
+		}
+
+		return true;
+	}
 }
